c1c07f42e36ee07b78b2feb11f1846e2be66c7ad
Reem Elsayed <reem.elsayed.14@ucl.ac.uk>
2019-02-14 15:54:23 -0500

    Fixed errors to show position

--- zceirel/www/js/userTracking.js
+++ zceirel/www/js/userTracking.js
@@ -9,16 +9,12 @@ function trackLocation() {
 	}
 
 // code to feed location onto map
-var userPos;
 function showPosition(position) {
-	// check if previous point exists
-	if (userMarker) {
-		mymap.removeLayer(userPos);
-	}
-	// add point on the map
-	userPos = L.marker([position.coords.latitude,position.coords.longitude]).addTo(mymap)
-	.bindPopup("<b>You were here</b>");
 	
-	mymap.setView([position.coords.latitude, position.coords.longitude], 13)
+	// add point on the map
+	L.marker([position.coords.latitude,position.coords.longitude]).addTo(mymap)
+	.bindPopup("<b>You were here</b>");
+
+	mymap.setView([position.coords.latitude, position.coords.longitude], 5);
 
 	}
